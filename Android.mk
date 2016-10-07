4fd1544b4fe63719a8ee797166a3c62b461b78f3
Alex Naidis <alex.naidis@linux.com>
2016-10-07 22:16:11 +0200

    Skia: enable SDCLANG LTO
    * together with QCLD
    
    Change-Id: I818b2d40f4dcb9a63dd3859eda89c0c463382d98
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- Android.mk
+++ Android.mk
@@ -43,6 +43,7 @@ LOCAL_PATH:= $(call my-dir)
 ###############################################################################
 
 include $(CLEAR_VARS)
+LOCAL_SDCLANG_LTO := true
 LOCAL_FDO_SUPPORT := true
 # This should be the last -Oxxx specified in LOCAL_CFLAGS
 LOCAL_CFLAGS += -O3
